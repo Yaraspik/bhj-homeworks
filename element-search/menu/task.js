46437cc646399a6c8b954210f54e9f5cef462897
Stepan <yaraspik@ya.ru>
2023-03-13 20:01:16 +0300

    Второе задание переделано

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -5,11 +5,17 @@ Array.from(document.querySelectorAll('.menu__link')).forEach(element => {
 function openSubMenu(event){
     if(this.nextElementSibling) {
         event.preventDefault();
+
+        if(this.nextElementSibling.classList.contains('menu_active')){
+            return this.nextElementSibling.classList.remove('menu_active');
+        }
+
         Array.from(document.querySelectorAll('.menu_sub')).forEach(element => {
             if(element.closest('body > .menu') === this.closest('body > .menu')){
                 element.classList.remove('menu_active');
             }
         });
+        
         this.nextElementSibling.classList.add('menu_active');
     }
 }
